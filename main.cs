4394ce713b7b60d18d0ad2c3b981b291a4afd617
abercr0mby <abercromby@gmail.com>
2020-01-03 16:25:48 +0000

    Day Two Part one

--- main.cs
+++ main.cs
@@ -3,13 +3,23 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+  /*  try{
+      var dayOne = new DayOne();
+      Console.WriteLine(dayOne.RunTestsAndGetResultPartOne());
+      Console.WriteLine(dayOne.RunTestsAndGetResultPartTwo());
+    }
+    catch(Exception ex){
+      Console.WriteLine(ex.Message);
+    } 
+  */
+
     try{
-    var dayOne = new DayOne();
-    Console.WriteLine(dayOne.RunTestsAndGetResultPartOne());
-    Console.WriteLine(dayOne.RunTestsAndGetResultPartTwo());
+      var dayTwo = new DayTwo();
+      Console.WriteLine(dayTwo.RunTestsAndGetResultPartOne());      
     }
     catch(Exception ex){
       Console.WriteLine(ex.Message);
     }
+
   }
 }
